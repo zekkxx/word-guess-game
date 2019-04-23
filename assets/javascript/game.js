3f43758f801b6737d8e10ca7219bf6b997ac7e00
Kieran 'zekkxx' Anthony <zekkxx@gmail.com>
2019-04-23 18:34:48 -0500

    Appied Javascript for game to page. --There was a secondary game file that didn't use the class structure I ended up implememnting, it was riddled with bugs and flaws and was never commited because of this. This is why the complete file was uploaded today.

---
+++ assets/javascript/game.js
@@ -0,0 +1,97 @@
+var constellationArray = ["andromeda", "aquarius", "aquila", "aries", "cancer", "canis-major", "capricornus", "carina", "centaurus", "cetus", "corona-borealis", "crater", "crux", "cygnus", "delphinus", "dorado", "draco", "eridanus", "gemini", "hercules", "hydra", "lacerta", "leo", "libra", "lupus", "lynx", "lyra", "orion", "pegasus", "perseus", "phoenix", "pisces", "sagittarius", "scorpius", "serpens-caput", "taurus", "ursa-major", "ursa-minor", "virgo"];
+var victoryCount=0;
+
+class Game {
+    constructor(){ //Constructs each new game to eradicate overlapping errors
+        this.chancesLeft=11;
+        this.guessedLetters="";
+        this.constellationName=this.assignWord();
+        this.playerHint=this.createPlayerHint();
+        console.log(this.constellationName + ":" + this.playerHint);
+    }
+
+    //This assigns a new word to the game for the player to guess, utilized only upon instantiation of a new game
+    assignWord(){
+        var index = Math.floor(Math.random()*constellationArray.length);
+        return constellationArray[index];
+    }
+
+    //This creates the hidden output for the player to reference, utilized only upon instantiation of a new game
+    createPlayerHint(){
+        var output="";
+        for(var i=0; i<this.constellationName.length; i++){
+            if(this.constellationName.substr(i, 1) == "-"){
+                output += "-"; //Places an - inbetween words to keep in line with pictures
+            } else {
+                output += "_"; //Replaces the letter with an '_'
+            }
+            output += " " //Adds a space between '_'s
+        }
+        return output;
+    }
+
+    //This is the update of all guess information, utilized any time a NEW and APPROVED key entity is pressed
+    updateFields(){
+        document.getElementById("wordSpan").innerHTML=this.playerHint;
+        document.getElementById("guessesLeftSpan").innerHTML="Chances Left: "+this.chancesLeft;
+        document.getElementById("guessedLettersSpan").innerHTML="Guessed Letters: "+this.guessedLetters;
+    }
+
+    //This is the main function of the game, awaiting information from a key press and submitting information based on the response.
+    verifyInput(input){
+        if(!/[a-z]/.test(input)){ //Test input against regular expression
+            document.getElementById("guessFeedbackSpan").innerHTML="This is not an approved letter. Try again.";
+        } else if(this.guessedLetters.search(input) != -1) { //Test input against Already Guessed Letters
+            document.getElementById("guessFeedbackSpan").innerHTML="This letter has already been guessed.";
+        } else {
+            document.getElementById("guessFeedbackSpan").innerHTML="";
+            this.compareInputWithName(input);
+            this.guessedLetters += input + ", "
+        }
+    }
+
+    //Compares the input value to the word and rewrites the output information
+    compareInputWithName(input){
+    var newWordOutput="";
+    var adjustmentMade = false;
+    for(var i=0; i<this.constellationName.length; i++){
+        if(this.constellationName.charAt(i)==input) { //If the point in the word has the input letter
+            newWordOutput += input + " ";
+            adjustmentMade = true;
+        } else {
+            newWordOutput += this.playerHint.charAt(2*i) + " "; //Due to spaces, i has to be doubled to get the proper next character
+        }
+    }
+    if(!adjustmentMade){
+        this.chancesLeft--;
+    }
+    this.playerHint = newWordOutput.toUpperCase(); //change the output to uppercase and save it back to playerHint
+}
+
+    checkForWinLoseConditions(){
+        if(this.chancesLeft<=0){ //If 0 or fewer guesses left, LOSE
+            this.newGame();
+        } else if(this.playerHint.search("_") == -1){ //If There are no '_' characters in the output, WIN
+            victoryCount+=1;
+            document.getElementById("themeRewardSpan").src="assets/images/"+this.constellationName+".jpg";
+            this.newGame();
+        }
+    }
+
+    newGame(){
+        myGame = new Game(); //Creates a new overriding Game element
+        document.getElementById("victoryCountSpan").innerHTML="Victories: "+victoryCount;
+    }
+}
+var myGame = new Game();
+window.addEventListener("keyup", startUp); //Start Screen
+
+function startUp(){
+    window.removeEventListener("keyup", startUp); //remove input element
+    myGame.updateFields(); //Move to player screen
+    window.addEventListener("keyup", function(event){ //replace input element and apply game loop.
+        myGame.verifyInput(event.key.toLowerCase());
+        myGame.checkForWinLoseConditions();
+        myGame.updateFields();
+    });
+}
