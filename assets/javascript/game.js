f8b272e8cd3d9ae0030118a192735a1239891037
Kieran 'zekkxx' Anthony <zekkxx@gmail.com>
2019-04-23 20:46:44 -0500

    Updated game.js to include the music cue.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,22 @@
 var constellationArray = ["andromeda", "aquarius", "aquila", "aries", "cancer", "canis-major", "capricornus", "carina", "centaurus", "cetus", "corona-borealis", "crater", "crux", "cygnus", "delphinus", "dorado", "draco", "eridanus", "gemini", "hercules", "hydra", "lacerta", "leo", "libra", "lupus", "lynx", "lyra", "orion", "pegasus", "perseus", "phoenix", "pisces", "sagittarius", "scorpius", "serpens-caput", "taurus", "ursa-major", "ursa-minor", "virgo"];
 var victoryCount=0;
 
+class Sound {
+    constructor(src) {
+        this.sound = document.createElement("audio");
+        this.sound.src = src;
+        this.sound.setAttribute("preload", "auto");
+        this.sound.setAttribute("controls", "none");
+        this.sound.style.display = "none";
+    }
+    play() {
+        this.sound.play();
+    }
+    stop() {
+        this.sound.pause();
+    }
+}
+
 class Game {
     constructor(){ //Constructs each new game to eradicate overlapping errors
         this.chancesLeft=11;
@@ -52,27 +68,29 @@ class Game {
 
     //Compares the input value to the word and rewrites the output information
     compareInputWithName(input){
-    var newWordOutput="";
-    var adjustmentMade = false;
-    for(var i=0; i<this.constellationName.length; i++){
-        if(this.constellationName.charAt(i)==input) { //If the point in the word has the input letter
-            newWordOutput += input + " ";
-            adjustmentMade = true;
-        } else {
-            newWordOutput += this.playerHint.charAt(2*i) + " "; //Due to spaces, i has to be doubled to get the proper next character
+        var newWordOutput="";
+        var adjustmentMade = false;
+        for(var i=0; i<this.constellationName.length; i++){
+            if(this.constellationName.charAt(i)==input) { //If the point in the word has the input letter
+                newWordOutput += input + " ";
+                adjustmentMade = true;
+            } else {
+                newWordOutput += this.playerHint.charAt(2*i) + " "; //Due to spaces, i has to be doubled to get the proper next character
+            }
         }
+        if(!adjustmentMade){
+            this.chancesLeft--;
+        }
+        this.playerHint = newWordOutput.toUpperCase(); //change the output to uppercase and save it back to playerHint
     }
-    if(!adjustmentMade){
-        this.chancesLeft--;
-    }
-    this.playerHint = newWordOutput.toUpperCase(); //change the output to uppercase and save it back to playerHint
-}
 
     checkForWinLoseConditions(){
         if(this.chancesLeft<=0){ //If 0 or fewer guesses left, LOSE
+            mySound.stop();
             this.newGame();
         } else if(this.playerHint.search("_") == -1){ //If There are no '_' characters in the output, WIN
             victoryCount+=1;
+            mySound.play();
             document.getElementById("themeRewardSpan").src="assets/images/"+this.constellationName+".jpg";
             this.newGame();
         }
@@ -84,8 +102,8 @@ class Game {
     }
 }
 var myGame = new Game();
+var mySound = new Sound("assets/sounds/Commerce_start.mp3")
 window.addEventListener("keyup", startUp); //Start Screen
-
 function startUp(){
     window.removeEventListener("keyup", startUp); //remove input element
     myGame.updateFields(); //Move to player screen
