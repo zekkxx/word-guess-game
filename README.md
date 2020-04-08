b56623b9d3ce1f56190589b1c47e621c984d408b
Kieran 'zekkxx' Anthony <zekkxx@gmail.com>
2020-04-07 23:16:32 -0500

    Updated ReadMe, and adjusted main files

--- README.md
+++ README.md
@@ -1,18 +1,21 @@
 # word-guess-game
-This project uses HTML, CSS and Javascript to create a word guessing game based on a theme.
+This project uses HTML, CSS and Javascript to create a word guessing game.
+Available at: https://zekkxx.github.io/word-guess-game/
 
-This project utilizes image files from external source: Sea and Sky - Fair Use: Educational/Not for Profit use
-This project utilizes a sound file from online game: Mabinogi - Fair Use: Educational/Not for Profit use
+## Explanation:
+This game is a word guessing game designed utilizing the Class structure. The intention behind this game was to utilize event listeners listening to the keyboard of a computer. As an additional goal, I wanted to include the capability to play the game from the browser even for devices without physical keyboards.
 
-The bulk of this project lays in the use of Javascript.
-Important notes:
-* 1. The name in the array directly corelates to the name of the picture in the assets/images directory. Changing the name in the array will therefore break the picture display afterwards unless the picture or the element in the array is renamed.
-* 2. Items can be added easily into the arrray by adding them at any point in the array. Please add a picture into the associated images file.
-* 3. This project uses a modified Object Oriented approach. The Game class may be removed and placed into linked file for clarity.
-* 4. Javascript notes have been added. Please refer to them when looking at the code.
-* 5. Project includes link to external repository -- This is my personal portfolio.
+## Usage Instructions:
+* Load the game in browser, and press any of button to begin the game.
+* Use the keyboard, or provided input system to guess the word hinted at on screen.
+* Continue until the word is guessed or you have run out of guesses!
 
-Quick Explanations:
-* 1. You will notice that I created a nested input reader function. This was done to add the start introduction screen. Make sure you retain the removal of the first input reader, as if you don't remove it, issues begin to crop up. These input readers didn't want to behave in the class itself, so I left them on the bottom as the instantiation of the Game itself.
-* 2. I have 2 global variables, the victories are per time the page is open, so it made sense to be outside of the Game class, and I felt it would be less intensive on the browser if it only had to load the array a single time. I have 4 class variables, and each of these were important variables that needed to be accessible by multiple functions, typically my updateFields function. The individual functions may include additional variables as per necessary for keeping track of internal variables.
-* 3. I chose to use the class structure for this program in order to ensure that no variables or data bled over outside of the victories. By creating a new game I was able to ensure that all of the old data could be erased and overwritten. In addition, despite some initial difficulties with functions, by wrapping all of the functions necessary inside the program, I was able to keep the class itself running most of the show.
+## Installation Process:
+* Fork Repository, and clone repo to machine.
+* Project can then be edited and run from a code editor.
+
+## Design Notation:
+
+
+## License Information:
+This project is liscensed under the Unliscense. More information is available in the `liscense.txt` file included in the repository, available here: https://github.com/zekkxx/word-guess-game.
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,5 @@
 var constellationArray = ["andromeda", "aquarius", "aquila", "aries", "cancer", "canis-major", "capricornus", "carina", "centaurus", "cetus", "corona-borealis", "crater", "crux", "cygnus", "delphinus", "dorado", "draco", "eridanus", "gemini", "hercules", "hydra", "lacerta", "leo", "libra", "lupus", "lynx", "lyra", "orion", "pegasus", "perseus", "phoenix", "pisces", "sagittarius", "scorpius", "serpens-caput", "taurus", "ursa-major", "ursa-minor", "virgo"];
-var victoryCount=0;
+var myGame;
 
 class Sound {
     constructor(src) {
@@ -8,6 +8,7 @@ class Sound {
         this.sound.setAttribute("preload", "auto");
         this.sound.setAttribute("controls", "none");
         this.sound.style.display = "none";
+        this.sound.volume = 0.2;
     }
     play() {
         this.sound.play();
@@ -18,24 +19,30 @@ class Sound {
 }
 
 class Game {
-    constructor(){ //Constructs each new game to eradicate overlapping errors
-        this.chancesLeft=11;
+    constructor(){
+        this.victoryCount=0;
+        this.victorySound = new Sound("assets/sounds/Commerce_start.mp3");
+        this.assignNewGameVariables();
+        this.updateFields();
+    }
+
+    assignNewGameVariables(){
+        this.guessesLeft=11;
         this.guessedLetters="";
         this.constellationName=this.assignWord();
         this.playerHint=this.createPlayerHint();
-        //console.log(this.constellationName + ":" + this.playerHint);
     }
 
     //This assigns a new word to the game for the player to guess, utilized only upon instantiation of a new game
     assignWord(){
-        var index = Math.floor(Math.random()*constellationArray.length);
+        let index = Math.floor(Math.random()*constellationArray.length);
         return constellationArray[index];
     }
 
     //This creates the hidden output for the player to reference, utilized only upon instantiation of a new game
     createPlayerHint(){
-        var output="";
-        for(var i=0; i<this.constellationName.length; i++){
+        let output="";
+        for(let i=0; i<this.constellationName.length; i++){
             if(this.constellationName.substr(i, 1) == "-"){
                 output += "-"; //Places an - inbetween words to keep in line with pictures
             } else {
@@ -49,7 +56,7 @@ class Game {
     //This is the update of all guess information, utilized any time a NEW and APPROVED key entity is pressed
     updateFields(){
         document.getElementById("wordSpan").innerHTML=this.playerHint;
-        document.getElementById("guessesLeftSpan").innerHTML="Chances Left: "+this.chancesLeft;
+        document.getElementById("guessesLeftSpan").innerHTML="Chances Left: "+this.guessesLeft;
         document.getElementById("guessedLettersSpan").innerHTML="Guessed Letters: "+this.guessedLetters;
     }
 
@@ -61,16 +68,18 @@ class Game {
             document.getElementById("guessFeedbackSpan").innerHTML="This letter has already been guessed.";
         } else {
             document.getElementById("guessFeedbackSpan").innerHTML="";
-            this.compareInputWithName(input);
+            this.compareInputWithWord(input);
             this.guessedLetters += input + ", "
+            this.checkEndConditions();
         }
+        this.updateFields();
     }
 
     //Compares the input value to the word and rewrites the output information
-    compareInputWithName(input){
+    compareInputWithWord(input){
         var newWordOutput="";
         var adjustmentMade = false;
-        for(var i=0; i<this.constellationName.length; i++){
+        for(let i=0; i<this.constellationName.length; i++){
             if(this.constellationName.charAt(i)==input) { //If the point in the word has the input letter
                 newWordOutput += input + " ";
                 adjustmentMade = true;
@@ -79,37 +88,35 @@ class Game {
             }
         }
         if(!adjustmentMade){
-            this.chancesLeft--;
+            this.guessesLeft--;
         }
         this.playerHint = newWordOutput.toUpperCase(); //change the output to uppercase and save it back to playerHint
     }
 
-    checkForWinLoseConditions(){
-        if(this.chancesLeft<=0){ //If 0 or fewer guesses left, LOSE
-            mySound.stop();
+    checkEndConditions(){
+        if(this.guessesLeft<=0){ //If 0 or fewer guesses left, LOSE
+            this.victorySound.stop();
             this.newGame();
         } else if(this.playerHint.search("_") == -1){ //If There are no '_' characters in the output, WIN
-            victoryCount+=1;
-            mySound.play();
+            this.victoryCount+=1;
+            this.victorySound.play();
             document.getElementById("themeRewardSpan").src="assets/images/"+this.constellationName+".jpg";
             this.newGame();
         }
     }
 
     newGame(){
-        myGame = new Game(); //Creates a new overriding Game element
-        document.getElementById("victoryCountSpan").innerHTML="Victories: "+victoryCount;
+        this.assignNewGameVariables();
+        this.updateFields();
+        document.getElementById("victoryCountSpan").innerHTML="Victories: "+this.victoryCount;
     }
 }
-var myGame = new Game();
-var mySound = new Sound("assets/sounds/Commerce_start.mp3")
+
 window.addEventListener("keyup", startUp); //Start Screen
 function startUp(){
+    myGame = new Game();
     window.removeEventListener("keyup", startUp); //remove input element
-    myGame.updateFields(); //Move to player screen
     window.addEventListener("keyup", function(event){ //replace input element and apply game loop.
         myGame.verifyInput(event.key.toLowerCase());
-        myGame.checkForWinLoseConditions();
-        myGame.updateFields();
     });
 }
